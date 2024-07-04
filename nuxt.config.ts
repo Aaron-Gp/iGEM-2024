9f317acf722cfebb75850bc35e6839410620c2e6
Peng Gao <peng.gao@bit.edu.cn>
2024-07-05 01:25:05 +0800

    Try to fix pages bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app:{
+    baseURL: '/bit/',
     buildAssetsDir: "/static/",
   },
   devtools: { enabled: true }
