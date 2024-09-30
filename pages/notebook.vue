d223e1bc6fee95eb38c19c52b483ca0e3dd1ab38
Yongyu Huo <2323187318@qq.com>
2024-09-30 12:09:15 +0000

    Add new file

---
+++ pages/notebook.vue
@@ -0,0 +1,12 @@
+<template>
+    
+</template>
+
+<script setup>
+useHead({
+    title: 'BIT iGEM 2024 - Inclusivity',
+    meta: [
+        { name: 'description', content: 'Welcome to the BIT Wiki' },
+    ],
+})
+</script>
